d94d9ae7ebd875338b36d22587c14d10b6faf55b
Larry McQueary <larry@mcqueary.org>
2026-08-23 14:48:45 -0600

    chore: remove obsolete development artifacts

--- bin/export-csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sqlite3 graves.db <<EOF
-.headers on
-.mode csv
-.output data.csv
-select * from graves;
-.quit
-EOF
--- bin/export-json.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sqlite3 $1 <<EOF
-.headers on
-.mode json
-.output data.json
-select * from graves;
-.quit
-EOF
